fa4deb1e8393a1be6d8da2d7ea4ab3718f87c935
seb1055 <sebmora1055@gmail.com>
2018-09-29 13:05:32 -0700

    Removed Chore class. Added Texter class and removed dup methods. Still need to work on sendVerification method to make it more general

---
+++ Texter.py
@@ -0,0 +1,39 @@
+from twilio.rest import Client
+import datetime
+
+
+class Texter:
+    account_sid = None
+    auth_token = None
+    number = None
+    client = None
+
+    def __init__(self, account_sid, auth_token, number):
+        self.account_sid = account_sid
+        self.auth_token = auth_token
+        self.number = number
+        client = client = Client(account_sid, auth_token)
+
+    def sendMessage(self, reccivernumber, message):
+        assert isinstance(reccivernumber)
+        message = self.client.messages \
+            .create(
+            body=message,
+            from_=self.number,
+            to=reccivernumber)
+
+    def sendChore(self, roommate):
+        date = datetime.datetime.today().weekday()
+
+        text = "%s \n Good Morning %s! \n \n The chore that you have been assigned today is: \n %s \n\n please respond DONE " \
+               "when you have completed the chore." % (date, roommate.name, roommate.chore)
+        print(
+                "%s \n Good Morning %s! \n The chore that you have been assigned today is: \n %s \n please respond "
+                "DONE when you have completed the chore." % (date, roommate.name, roommate.chore))
+        message = self.client.messages \
+            .create(
+            body=text,
+            from_=self.number,
+            to=roommate.number
+        )
+

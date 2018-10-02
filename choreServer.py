a55ade820c96d4c7a82e0c40e00b99e1a2f20bcd
seb1055 <sebmora1055@gmail.com>
2018-10-02 15:12:58 -0700

    Fixed Logic error and make input all lowercase

--- choreServer.py
+++ choreServer.py
@@ -7,7 +7,7 @@ from flask import Flask, request
 import random
 import time
 
-roommates = [Roommate("Seb", "+17072257532", [6, 3],[]), Roommate("Ed", "+17072878986", [6, 2],[])]
+roommates = [Roommate("Seb", "+17072257532", [1, 3],[]), Roommate("Ed", "+17072878986", [6, 1],[])]
 
 #Roommate("Jake", "+15593080259", [1, 3]),Roommate("Chase","+18053387701",[1,6]),]
 
@@ -139,12 +139,12 @@ def sms_reply(sender,message_body):
                     verificationlist.append(roommate.number)
                 texter.sendMessage(sender.number, "Your request is being processed by your roommates")
 
-    elif ("YES" in message_body and sender.number in verificationlist):
+    elif ("yes" in message_body.lower() and sender.number in verificationlist):
 
         try:
             name = message_body.split()
             name = name[1]
-            if (any(roommate.name == sender.name for roommate in roommates)):  # if the name found is not a person doing chores
+            if (not any(roommate.name.lower() == name.lower() for roommate in roommates)):  # if the name found is not a person doing chores
                 raise Exception
 
         except:

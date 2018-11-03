c839c06fbb63f33d8d460d29fffb0758f8dafdea
seb1055 <sebmora1055@gmail.com>
2018-11-03 14:17:56 -0700

    moving to make chorebot handel multi apartments. Need to finish JSON Parser

---
+++ Apartment.py
@@ -0,0 +1,13 @@
+from ChoreManager import ChoreManager
+
+
+class Apartment:
+
+    def __init__(self, roommates, weeklyChores, recurringChores,choretime,remindertime):
+        self.choremanager = ChoreManager(weeklyChores, recurringChores)
+        self.roommates = roommates
+        self.choretime = choretime
+        self.remindertime = remindertime
+
+    def addRoommate(self,roommate):
+        self.roommates.append(roommate)

10537d277ea17c7d18204f3c52ea3a95c9449b2e
Seb <sebmora1055@gmail.com>
2019-01-28 17:04:18 -0800

    Updated days and fixed indentation

--- JsonParser.py
+++ JsonParser.py
@@ -12,17 +12,14 @@ class JsonParser:
     def parseApartments(self):
         apartments = []
         for i in range(0, int(self.data["len"])): #ghetto way
-           apt = "Apt_%d" % i
-           aptname =  self.data[apt]["Apt_name"]
-           roommates = self.parseRoommates(apt)
-           weekleychores = self.data[apt]["chores"][0]["weekly_chores"]
-           recchores = self.data[apt]["chores"][0]["recurring_chores"]
-           choretime = self.data[apt]["assign-chore-time"]
-           remnindertime = self.data[apt]["chore-reminder-time"]
-
-
-           apartments.append(Apartment(aptname,roommates,weekleychores,recchores,choretime,remnindertime))
-
+            apt = "Apt_%d" % i
+            aptname =  self.data[apt]["Apt_name"]
+            roommates = self.parseRoommates(apt)
+            weekleychores = self.data[apt]["chores"][0]["weekly_chores"]
+            recchores = self.data[apt]["chores"][0]["recurring_chores"]
+            choretime = self.data[apt]["assign-chore-time"]
+            remnindertime = self.data[apt]["chore-reminder-time"]
+            apartments.append(Apartment(aptname,roommates,weekleychores,recchores,choretime,remnindertime))
         return apartments
 
 
@@ -30,7 +27,7 @@ class JsonParser:
         roommates = []
         x= len(self.data[apt]["roommates"])
         for i in range(0, x):  # ghetto way
-            name  = self.data[apt]["roommates"][i]["name"]
+            name = self.data[apt]["roommates"][i]["name"]
             number = self.data[apt]["roommates"][i]["number"]
             status = self.data[apt]["roommates"][i]["completionPending"]
 
@@ -40,6 +37,7 @@ class JsonParser:
 
             chores = []
             for chore in self.data[apt]["roommates"][i]["chores"]:
+                assert isinstance(chore, object)
                 chores.append(chore)
 
             roommates.append(Roommate(name,number,days,chores))
